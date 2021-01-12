2d5ba99a595be337d7af7739ffbdeb1dfe5734c7
Albert Zenuni <az41597@ubt-uni.net>
2021-01-12 18:15:31 +0100

    EmployeeCreateViewModel & Data Input Validation

--- Paycompute/Controllers/EmployeeController.cs
+++ Paycompute/Controllers/EmployeeController.cs
@@ -31,5 +31,9 @@ namespace Paycompute.Controllers
             }).ToList();
             return View(employees);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
